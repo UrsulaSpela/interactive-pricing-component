2a41b7ce676a3b8b2ddf87e63ba68631457a2051
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-06-03 23:28:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-
 var slider = document.querySelector('#myRange');
 var monthlySwitch = document.querySelector("input[type=checkbox]");
 var valueViews = document.querySelector('#valueViews');
 var valuePrice = document.querySelector('#valuePrice');
 
 valueViews = ["10K", "50K", "100K", "500K", "1M"];
+valuePrice = ["$8", "$12", "$16", "$24", "$36"];
 
 slider.max = valueViews.length;
 document.querySelector("input[type=checkbox]").addEventListener("click", switchState);
@@ -23,5 +23,6 @@ slider.oninput = function() {
     i = this.value;
     document.querySelector("#valuePrice").innerText = valuePrice[i-1];
     document.querySelector("#valueViews").innerText = valueViews[i-1];
+    i = this.value;
     var color = 'linear-gradient(90deg, hsl(174, 86%, 45%)' + ((i * 25) - 25)  + '%, hsl(224, 65%, 95%) 1px';
 }
